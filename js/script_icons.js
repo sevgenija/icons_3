0d6422540271433c904e4d2e505e484fcd120e3b
Evgenia <sevgenija@gmail.com>
2019-11-06 19:08:52 +0100

    icons_3

---
+++ js/script_icons.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+$("#button").click(function() {
+  $('.transform').toggleClass('transform-active');
+});
+
+$("#button").click(function() {
+  $('.transform1').toggleClass('transform1-active');
+});
+
+$("#button").click(function() {
+  $('.transform2').toggleClass('transform2-active');
+});
+
+$("#button").click(function() {
+  $('.transform3').toggleClass('transform3-active');
+});
+
+$("#button").click(function() {
+  $('.transform4').toggleClass('transform4-active');
+});
